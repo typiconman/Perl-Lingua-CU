c53d97b0ba8d87e897dadcef5df57fe8dbfab5e2
Aleksandr Andreev <aleksandr.andreev@gmail.com>
2015-12-30 11:06:44 +0300

    Adding list of hyphenated words

---
+++ lib/Lingua/CU/Hyphenate.pm
@@ -0,0 +1,33 @@
+package Lingua::CU::Hyphenate;
+
+use strict;
+use utf8;
+
+our $VERSION = '0.01';
+
+1;
+
+=head1 NAME
+
+Lingua::CU::Hyphenate - Provides a hyphenation and syllabification algorithm for Church Slavonic.
+
+=head1 DESCRIPTION
+
+This is presently just a wrapper for some data used to create Church Slavonic hyphenation support.
+
+=head1 SEE ALSO
+
+perl(1), TeX::Hyphen.
+
+=head1 AUTHOR
+
+Aleksandr Andreev L<aleksandr.andreev@gmail.com>.
+
+=head1 LICENSING
+
+Copyright (c) 2015 Aleksandr Andreev (http://sci.ponomar.net/)
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.14.2 or,
+at your option, any later version of Perl you may have available.
+
